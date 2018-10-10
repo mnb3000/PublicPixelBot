e089b4ff428800fe00d32cd7855dbeb84d6c544c
mnb3000 <misha.mykolyshyn@gmail.com>
2018-10-11 00:16:28 +0300

    Валидация паблика и картинки

--- Back.js
+++ Back.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
+const path = require('path');
 const fastify = require('fastify')({
   logger: true,
-  key: fs.readFileSync(path.join(__dirname, 'domain.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'chechnya.cert'))
+  /*https: {
+    key: fs.readFileSync(path.join(__dirname, 'domain.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'chechnya.cert')),
+  },*/
 });
+
+const allowedPublics = ["22751485"];
+const allowedImageUrl = "http://shampinion.cf/controlImage.png";
 
 const startOpts = {
   schema: {
     body: {
       type: 'object',
       properties: {
-        url: 'string',
-        imageUrl: "string",
-        userId: "string"
-      }
-    }
-  }
+        url: {type: 'string'},
+        imageUrl: {type: 'string'},
+        userId: {type: 'string'},
+      },
+      required: ["url", "imageUrl", "userId"],
+    },
+  },
 };
 
-fastify.post('/start', (request, reply) => {
+fastify.post('/start', startOpts, (request, reply) => {
+  const url = request.body.url;
+  const publicId = url.match(/&group_id=(\d+)/)[1];
+  if (allowedPublics.includes(publicId) && request.body.imageUrl === allowedImageUrl) {
+    reply.send({ok: true});
+  } else {
+    reply.send({ok: false});
+  }
+});
 
+fastify.listen(8080, (err, address) => {
+  if (err) throw err;
+  fastify.log.info(`server listening on ${address}`)
 });
